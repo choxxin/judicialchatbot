771110cc68fe9a3953b52abe2066d2707a10884f
Ansh <132185361+choxxin@users.noreply.github.com>
2025-12-09 21:00:04 +0530

    ff

--- server/index.js
+++ server/index.js
@@ -1,50 +1,50 @@
-require('dotenv').config()
-const express=require('express')
-const PORT=process.env.PORT || 3000
-const app=express()
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
 //cors setup for cross origin requests
-const cors=require('cors')
-app.use(cors(
-    {
-        origin: [process.env.FRONTEND_URL],
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
-        secure: false
-    }
-))
+const cors = require("cors");
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    secure: false,
+  })
+);
 
 //cookie parser
-const cookiesParser=require('cookie-parser')
-app.use(cookiesParser({
+const cookiesParser = require("cookie-parser");
+app.use(
+  cookiesParser({
     secure: false,
-    sameSite: 'lax'
-}))
-
+    sameSite: "lax",
+  })
+);
 
 //enables parsing json response
-app.use(express.json())
-
+app.use(express.json());
 
 //api endpoints
-const router=require('./routes/index.js')
-app.use('/api',router)
+const router = require("./routes/index.js");
+app.use("/api", router);
 
 //for deploying
-const path=require('path');
+const path = require("path");
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-
 //db connection
-const connectDB=require('./db/connectDb')
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`app running on http://localhost:${PORT}/`);
-    })
-})
-.catch((err) => {
+const connectDB = require("./db/connectDb");
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`app running on http://localhost:${PORT}/`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
+  });
